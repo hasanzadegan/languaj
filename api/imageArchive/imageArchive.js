6d7f710d9b2b5c8712d7aa8fe387d087cab8fcca
hasanz <sh.h.zadegan@gmail.com>
2021-05-19 11:37:48 +0430

    how to setup

--- api/imageArchive/imageArchive.js
+++ api/imageArchive/imageArchive.js
@@ -2,10 +2,11 @@ const query = require('../helpers/query');
 module.exports = {
     getImage: async function (docId) {
         stmt = 'select docData from docArchive where docArchiveId ='+docId;
-        // console.log(stmt)
+        // console.log(stmt***)
         return await query(stmt, '').then(function (result) {
             if(result.length>0)
                 return result[0];
+
         })
     },
 }
