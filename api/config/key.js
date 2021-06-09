824dc923534f8b4905611f6fee515d1402e5a75a
hasanz <sh.h.zadegan@gmail.com>
2021-06-09 13:29:12 +0430

    nginex:oauth

--- api/config/key.js
+++ api/config/key.js
@@ -2,7 +2,7 @@ module.exports = {
     google:{
         clientID:'626109221196-hjsq35vp8jti3u6q37524rsan46vf5vu.apps.googleusercontent.com',
         clientSecret:'-tlnGNDmTIduzllgyn-_hoPd',
-        callbackURL:'/auth/google/redirect'
+        callbackURL:'/api/auth/google/redirect'
     },
     session:{
         cookieKey:"cookie_key_set_up_by_you"
