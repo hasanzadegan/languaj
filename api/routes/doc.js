4c6e9e3c0ed2e27853b116207e84111caff61c87
hasanz <sh.h.zadegan@gmail.com>
2021-06-09 14:05:52 +0430

    nginex:docArchi

--- api/routes/doc.js
+++ api/routes/doc.js
@@ -2,7 +2,7 @@ const imageArchive = require('../imageArchive/imageArchive');
 const docArchive = require('../docArchive/docArchive');
 
 module.exports = function(app){
-    app.get("/doc/:id", function (req, res) {
+    app.get("/api/media/:id", function (req, res) {
         imageArchive.getImage(req.params.id).then(result => {
             if (result.docData) {
                 var img = Buffer.from(result.docData.split(',')[1], 'base64');
@@ -18,8 +18,27 @@ module.exports = function(app){
             }
         }).catch(err => {
             res.send('');
-        })
+        });
     });
+    //
+    // app.get("/doc/:id", function (req, res) {
+    //     imageArchive.getImage(req.params.id).then(result => {
+    //         if (result.docData) {
+    //             var img = Buffer.from(result.docData.split(',')[1], 'base64');
+    //             res.writeHead(200, {
+    //                 'Content-Type': result.docData.split(',')[0].split('data:')[1].split(';')[0],
+    //                 'Cache-Control':'public',
+    //                 'Accept-Ranges': 'bytes',
+    //                 'max-age':'31536000',
+    //                 'Content-Length': img.length
+    //             });
+    //             res.end(img);
+    //         } else {
+    //         }
+    //     }).catch(err => {
+    //         res.send('');
+    //     })
+    // });
 
     app.post('/api/saveDoc', (req, res) => {
         docArchive.saveDoc(req.body).then(function (result) {
