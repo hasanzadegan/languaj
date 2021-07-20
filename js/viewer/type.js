9f97910b87e8e127906d6b68662f64fa5be76d8b
hasanz <sh.h.zadegan@gmail.com>
2021-07-20 15:35:07 +0430

    site multilingual: clear cache

--- js/viewer/type.js
+++ js/viewer/type.js
@@ -1,13 +1,12 @@
 angular.module('myApp').controller('typeController',
     function ($rootScope, $scope, $q, $timeout, WordService, BaeService, StorageService, LessonService, SoundService) {
-
-
+        $scope.current.typeText = '';
         $scope.func.checkType = function () {
-            if($scope.current.selectedCourse.selectedLesson.selectedTopic.selectedLevel.levelLexicalPhraseList[0].title===$scope.current.typeText){
+            console.log("$scope.current.viewerData.questionItem",$scope.current.viewerData.questionItem)
+            if($scope.current.viewerData.questionItem===$scope.current.typeText){
                 $scope.func.isCorrect();
             } else {
                 $scope.func.isInCorrect();
             }
         }
-
     });
